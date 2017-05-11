29210a92281ea694d677a76c1ec82137eb3b3322
zyro <zyro@zyro.net>
2017-05-11 17:43:02 +0200

    sample app reproducing grails/grails-data-mapping#949

--- grails-app/init/myapp/BootStrap.groovy
+++ grails-app/init/myapp/BootStrap.groovy
@@ -1,8 +1,12 @@
 package myapp
 
+import org.grails.datastore.gorm.GormEnhancer
+import org.grails.orm.hibernate.AbstractHibernateGormStaticApi
+
 class BootStrap {
 
     def init = { servletContext ->
+        ((AbstractHibernateGormStaticApi) GormEnhancer.findStaticApi(Dummy)).findAllWithSql("select * from Dummy", [max: 10])
     }
     def destroy = {
     }
